687b4e069366101cd2ddc939703957d223764e24
kimyoo04 <58503130+kimyoo04@users.noreply.github.com>
2024-05-01 12:25:40 +0900

    ui component 파일만 생성

---
+++ src/ui/Divider.tsx
@@ -0,0 +1,3 @@
+export default function Divider({ children }: { children: React.ReactNode }) {
+  return <div className="">{children}</div>
+}
---
+++ src/ui/Stack.tsx
@@ -0,0 +1,3 @@
+export default function Stack({ children }: { children: React.ReactNode }) {
+  return <div className="">{children}</div>
+}
